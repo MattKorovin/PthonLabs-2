f93251d60d2ce987f6e4410831d08c8ea5a4cea5
admin <dsag@>
2026-07-06 14:02:11 +0300

    123

--- LAB9/LAB9.py
+++ LAB9/LAB9.py
@@ -1,1 +1,14 @@
-from flask_sqlalchemy import SQLAlchemy
+from flask import request
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+
+    if request.method == "POST":
+
+        city = request.form["city"]
+        date = request.form["date"]
+
+        print(city)
+        print(date)
+
+    return render_template("index.html")
